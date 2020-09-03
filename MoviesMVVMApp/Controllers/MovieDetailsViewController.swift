f030087d24c737501f36d5399e0f66a731c095c7
Kobl <ackzackz@gmail.com>
2020-09-03 14:15:20 +0300

    feat: add navigation bar to movie details controller

--- MoviesMVVMApp/Controllers/MovieDetailsViewController.swift
+++ MoviesMVVMApp/Controllers/MovieDetailsViewController.swift
@@ -56,16 +56,37 @@ class MovieDetailsViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.isNavigationBarHidden = false
+        setupNavigationBar()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         print(viewModel.rating)
-        // Do any additional setup after loading the view.
+        
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().tintColor = .white
+        
     }
     
     // MARK: - Private methods
+    
+    private func setupNavigationBar() {
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.backItem?.title = ""
+        
+        let likeButton = UIButton()
+        likeButton.addTarget(self, action: #selector(addToFavorites), for: .touchUpInside)
+        likeButton.setImage(UIImage(systemName: "heart.fill", withConfiguration: UIImage.SymbolConfiguration(textStyle: .title2)), for: .normal)
+        let shareButton = UIButton()
+        shareButton.addTarget(self, action: #selector(shareMovie), for: .touchUpInside)
+        shareButton.setImage(UIImage(systemName: "square.and.arrow.up", withConfiguration: UIImage.SymbolConfiguration(textStyle: .title2)), for: .normal)
+        let stackView = UIStackView(arrangedSubviews: [likeButton, UIView(), shareButton])
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: stackView)
+    }
     
     private func setupViews() {
         self.view.addSubview(backgroundImageView)
@@ -78,12 +99,22 @@ class MovieDetailsViewController: UIViewController {
         
         self.view.addSubview(movieImageView)
         
-        NSLayoutConstraint.activate([movieImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+        NSLayoutConstraint.activate([movieImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
                                      movieImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
                                      movieImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
                                      movieImageView.heightAnchor.constraint(equalTo: movieImageView.widthAnchor, multiplier: 1.4)
         ])
         
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func shareMovie() {
+        print("Share")
+    }
+    
+    @objc private func addToFavorites() {
+        print("Added to Favorites")
     }
 
 }
